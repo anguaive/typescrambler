dfad9724cf62f031a911cd70b743ba0888e59a8e
anguaive <angurg98@gmail.com>
2019-11-29 22:28:39 +0100

    feat: implement main menu and empty activities

---
+++ app/src/main/java/com/angu/myapplication/MenuActivity.java
@@ -0,0 +1,55 @@
+package com.angu.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        final Button buttonNewGame = findViewById(R.id.buttonNewGame);
+        final Button buttonHighscores = findViewById(R.id.buttonHighscores);
+        final Button buttonSettings = findViewById(R.id.buttonSettings);
+
+        // log screen size in dp
+        DisplayMetrics displayMetrics = this.getResources().getDisplayMetrics();
+        float dpHeight = displayMetrics.heightPixels / displayMetrics.density;
+        float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
+        System.out.println("Screen size in dp: " + dpWidth + " * " + dpHeight);
+
+        buttonNewGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonHighscores.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, HighscoreActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
